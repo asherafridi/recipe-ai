0ca27af19eb979d4acbdfa241939779a1f25bd11
Asher Afridi <ashirchannel765@gmail.com>
2024-04-25 23:58:26 +0500

    Contact Module Updated

--- app/api/contacts/[id]/route.tsx
+++ app/api/contacts/[id]/route.tsx
@@ -2,10 +2,8 @@ import prisma from "@/lib/db";
 import { getServerSession } from "next-auth";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== 'GET') {
-        return res.status(405).end(); // Method Not Allowed
-    }
+export default async function GET(req: NextApiRequest, res: NextApiResponse) {
+    
 
     const { id } = req.query;
 
