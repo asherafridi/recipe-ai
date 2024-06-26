8267291632f7e16676f77dd6d006476096bc819e
Asher Afridi <ashirchannel765@gmail.com>
2024-06-26 15:50:19 +0500

    deploy

--- app/(auth)/verify/email/page.jsx
+++ app/(auth)/verify/email/page.jsx
@@ -1,27 +1,28 @@
 "use client"
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import Breadcrumb from '@/components/Breadcrumb';
 import axios from 'axios';
 import toast from 'react-hot-toast';
-import { Suspense } from 'react'
-
 
 const Page = () => {
   const params = useSearchParams();
   const token = params.get('token');
   const router = useRouter();
 
-  axios.post('/api/auth/verify', {token:token})
-  .then(response=>{
-    toast.success(response.data.msg);
-    setTimeout(()=>{
-        router.push('/sign-in');
-    },2000)
-  }).catch(e=>{
-    toast.error(e.response.data.msg);
-  })
-
+  useEffect(() => {
+    if (token) {
+      axios.post('/api/auth/verify', { token })
+        .then(response => {
+          toast.success(response.data.msg);
+          setTimeout(() => {
+            router.push('/sign-in');
+          }, 2000);
+        })
+        .catch(e => {
+          toast.error(e.response.data.msg || 'An error occurred.');
+        });
+    }
+  }, [token, router]);
 
   return (
     <div className='p-5 min-h-screen'>
