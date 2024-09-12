20e2f80c742883660a97d20fd53ee9567b6db8b3
asherafridi <ashirchannel765@gmail.com>
2024-09-12 21:25:22 +0500

    UI Changed

--- app/(dashboard)/credit/page.tsx
+++ app/(dashboard)/credit/page.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 const Page = () => {
   return (
     <div className='p-5 min-h-screen'>
-        <Breadcrumb title="Purchase More Credits" />
         <div className="grid grid-cols-4 mt-10 gap-5">
         </div>
     </div>
